d004e1a1f83c2d00df88647da0edb0b73189fed7
Michal Lopuszynski <lopusz@users.noreply.github.com>
2015-07-13 19:16:54 +0200

    Adding tests included in the original implementation (PolishLight  stemmer)

--- test/clef_tools/core_test.clj
+++ test/clef_tools/core_test.clj
@@ -1,6 +1,20 @@
 (ns clef_tools.core-test
-  (:require [clojure.test :refer :all]
-            [clef_tools.core :refer :all]))
+  (:require [clojure.test :refer :all])
+  (:import [clef_tools.stemmers PolishLight]))
 
 (deftest simple-test
-  (is (= 1 2)))
+  (let [
+        ns (PolishLight.)
+    ]
+  (is (= (.stem ns "panowie") "pan"))
+  (is (= (.stem ns "aktorowi") "aktor"))
+  (is (= (.stem ns "kotami") "kot"))
+  (is (= (.stem ns "kotach") "kot"))
+  (is (= (.stem ns "wodżie") "wod"))
+  (is (= (.stem ns "autobuśem") "autobus"))
+  (is (= (.stem ns "aktorom") "aktor"))
+  (is (= (.stem ns "kotow") "kot"))
+  (is (= (.stem ns "aktokze") "aktok"))
+  (is (= (.stem ns "aktorzy") "aktor"))
+  (is (= (.stem ns "kotami") "kot"))
+  (is (= (.stem ns "Rośłjanin") "Rosljan"))))

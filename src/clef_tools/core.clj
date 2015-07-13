029c9bf83ddf0652a024ad62c1a15b2431301682
Michal Lopuszynski <lopusz@users.noreply.github.com>
2015-07-13 19:46:11 +0200

    Adding scaffolding for stopwords import

--- src/clef_tools/core.clj
+++ src/clef_tools/core.clj
@@ -1,7 +1,10 @@
-(ns clef_tools.core
+(ns clef-tools.core
   (:refer-clojure :exclude [assert])
+  (:import [clef_tools.stopwords Polish])
   (:gen-class))
 
+(defn pl-get-stopwords-clef-short []
+  (into #{} Polish/listShort))
 
 (defn -main 
   "Main function of the project."
---
+++ src/clef_tools/stopwords/Polish.java
@@ -0,0 +1,14 @@
+package clef_tools.stopwords;
+
+public final class Polish {
+
+    private Polish() {};
+
+    public static final String [] listShort = {
+            "i",
+            "lub",
+            "nie",
+            "tak"
+    };
+
+}
